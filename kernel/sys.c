fa56801aa805045ee27a3a10c7bdaf2e5a978370
Amrito Karmokar <amritokarmokar5i@gmail.com>
2024-12-16 04:22:19 +0000

    Revert "syscall: Fake uname to 4.19 also for netbpfload"
    This reverts commit 4d77aa0b7529edf18e018fa9b26ba48ad58db197.

--- kernel/sys.c
+++ kernel/sys.c
@@ -1214,7 +1214,6 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	memcpy(&tmp, utsname(), sizeof(tmp));
 #ifdef CONFIG_ANDROID_SPOOF_KERNEL_VERSION_FOR_BPF
 	if (!strncmp(current->comm, "bpfloader", 9) ||
-	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
 		strcpy(tmp.release, "4.19.0");
 		pr_debug("fake uname: %s/%d release=%s\n",
